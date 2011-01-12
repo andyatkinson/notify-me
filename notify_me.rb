81cb49b4d7ac5c9adc882be75a9fe2ea0f0764e6
Andy Atkinson <andy@webandy.com>
2011-01-11 21:58:45 -0500

    fixing column types

--- notify_me.rb
+++ notify_me.rb
@@ -7,8 +7,8 @@ set :database, ENV['DATABASE_URL'] || 'sqlite://subscriptions.db'
 migration "create subscriptions" do
   database.create_table :subscriptions do
     primary_key :id
-    string      :email, :null => false
-    timestamp   :created_at, :null => false
+    String      :email,      :null => false
+    DateTime    :created_at, :null => false
   end
 end
 
